2c7fd3414fd71dc37b22048fcc3de00b62d38481
Prabesh <prabeshacharya5678@gmail.com>
2025-12-03 07:21:47 +0545

    backend configured

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.config.js";
+
+const app=express();
+const port = 3000;
+
+await connectDB();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // required for cookies/auth
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.get("/", (_, res) => res.send("API is working"));
+
+// ✅ Start server
+app.listen(port, () => {
+  console.log(`🚀 Server is running on http://localhost:${port}`);
+});
