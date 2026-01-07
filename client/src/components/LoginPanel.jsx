ba5fae6fe910318dd9fc9c6751ff0306438d207b
Sajana54 <sajanaranjitkar64@gmail.com>
2026-01-07 20:02:08 +0545

    LoginPanel.jsx

--- client/src/components/LoginPanel.jsx
+++ client/src/components/LoginPanel.jsx
@@ -6,11 +6,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 const ADMIN_EMAIL = "deekshyabadal@gmail.com";
-const TEACHER_EMAIL = ["subhechhakarkee@gmail.com",
-"sajanaranjitkar64@gmail.com",
-"prabesh5678@gmail.com",
-"deekshyabadal@gmail.com",
-];
+const TEACHER_EMAIL = "sajanaranjitkar64@gmail.com";
 
 const LoginPanel = () => {
   const { setUser, showUserLogin, setShowUserLogin } = useAppContext();
