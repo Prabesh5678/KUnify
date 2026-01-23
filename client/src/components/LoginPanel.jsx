b770edfde06e48dbfb734f17d3eaa2a071a157cb
Sajana54 <sajanaranjitkar64@gmail.com>
2026-01-23 18:57:24 +0545

    khai k ho

--- client/src/components/LoginPanel.jsx
+++ client/src/components/LoginPanel.jsx
@@ -12,14 +12,14 @@ axios.defaults.withCredentials = true;
 const ADMIN_EMAIL = [
   "ssubhechhakarkee@gmail.com",
    "deekshyabadal@gmail.com",
-   "sajanaranjitkar64@gmail.com",
+   "ranjitkarsajana@gmail.com",
 ];
 
 const TEACHER_EMAIL = [
   "grocerease6699@gmail.com",
   "ddeekshyabadal@gmail.com",
   "subhechhakarkee@gmail.com",
-  "ssajanaranjitkar64@gmail.com"
+  "sajanaranjitkar64@gmail.com"
 ];
 
 const LoginPanel = () => {
